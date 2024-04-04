37a54b5f75b71c61e8756362fad62cec959dd316
Larry <tradesouthwest@gmail.com>
2024-04-04 09:47:42 -0700

    rm file

--- admin/partials/views/string.php
+++
@@ -1,8 +0,0 @@
-<?php 
-printf(
-    '<input name="%1$s[%2$s]" id="%3$s" value="%4$s" class="regular-text">',
-    $args['option_name'],
-    $args['name'],
-    $args['label_for'],
-    $args['value']
-);
